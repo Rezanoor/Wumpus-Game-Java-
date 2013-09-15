4e84a2946525362221f4ed0707daeb497bd50f34
Reza <rezanoorbakhsh@ymail.com>
2013-09-15 17:07:13 +1000

    Create Bats.java
    Implementation of Bats class

---
+++ Bats.java
@@ -0,0 +1,22 @@
+package WumpusGame;
+
+import WumpusGame.Edge;
+
+public class Bats {
+  
+   public String type;
+   public String room_ID;
+   public int quantity;
+    
+    public Bats(String t,String r,int q) {
+        
+       type = t;
+       room_ID = r;
+       quantity = q;
+    }
+    public Bats() {
+        
+       
+    }
+		
+}
